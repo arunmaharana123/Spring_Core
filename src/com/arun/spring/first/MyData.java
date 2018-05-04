62eb89728a4153aae265bd8c83149de0cb4cce13
Arun Maharana <arun.maharana@yahoo.in>
2018-05-04 14:38:02 +0530

    First Example
    Bean

---
+++ src/com/arun/spring/first/MyData.java
@@ -0,0 +1,13 @@
+package com.arun.spring.first;
+
+public class MyData {
+	private String data = "MyDATA";
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public void getData() {
+		System.out.println(data);
+	}
+}
---
+++ src/com/arun/spring/first/Test.java
@@ -0,0 +1,15 @@
+package com.arun.spring.first;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
+		MyData myData = (MyData) factory.getBean("mydata");
+		myData.getData();
+
+	}
+
+}
